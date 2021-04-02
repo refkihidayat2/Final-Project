6e693ab47744844a15b667a34f805968e9479d18
refkihidayat2 <refkihidayat29@gmail.com>
2021-04-02 11:35:16 +0700

    final project

--- Final_Project.py
+++ Final_Project.py
@@ -1,25 +1,27 @@
 import smtplib
-import os
 
 server = smtplib.SMTP("smtp.gmail.com", 587)
 server.ehlo()
 server.starttls()
 
-sender_mail = "refkihidayat29@gmail.com"
+sender_mail = input(str("Masukkan email : "))
 password = input(str("Masukkan password : "))
 
 server.login(sender_mail, password)
 print("Login sukses")
 
-receiver_list = open("receiver_list.txt", "r")
-receiver_mail = len(receiver_list.read())
 
+receiver_list = open("receiver_list.txt", "r")
+receiver_mail = [(line.strip()).split() for line in receiver_list]
+
+print(receiver_mail)
 subject = input(str("Masukkan subject : "))
 message = input(str("Masukkan isi pesan : "))
 
 msg = F"Subject: {subject}\n\n{message}"
+for mail in receiver_mail:
+    server.sendmail(sender_mail, mail, msg)
 
-server.sendmail(sender_mail, receiver_mail, msg)
 print("Pesan terkirim")
 
 
